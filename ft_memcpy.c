4901d96397d053a23b111fb1beae2e7b51134c51
Ruben Nuno Imbroda <rnuno-im@c2r12s1.42madrid.com>
2025-10-01 15:25:33 +0200

    correccion de errores y añadir nuevas funciones(19 en total)

---
+++ ft_memcpy.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rnuno-im <rnuno-im@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/01 14:40:10 by rnuno-im          #+#    #+#             */
+/*   Updated: 2025/10/01 15:06:53 by rnuno-im         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char	*destino;
+	unsigned char	*origen;
+	size_t			i;
+
+	i = 0;
+	destino = (unsigned char *)dest;
+	origen = (unsigned char *)src;
+	while (i < n)
+	{
+		destino[i] = origen[i];
+		i++;
+	}
+	return (dest);
+}
+
+int main()
+{
+	char src[] = "me llamo ruben";
+	char dest[20];
+
+	ft_memcpy(dest, src, 12);
+	dest[12] = '\0';
+	printf("%s\n", dest);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: rnuno-im <rnuno-im@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 14:37:23 by rnuno-im          #+#    #+#             */
-/*   Updated: 2025/09/30 19:05:19 by rnuno-im         ###   ########.fr       */
+/*   Updated: 2025/10/01 14:55:11 by rnuno-im         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,21 @@
 #include <stdio.h>
 
 size_t	ft_strlen(const char str[]);
+int	ft_atoi(const char *nptr);
+void	ft_bzero(void *s, size_t n);
+int	ft_isalnum(char c);
+int	ft_isalpha(char c);
+int	ft_isascii(int c);
+int	ft_isdigit(char c);
+int	ft_isprint(int c);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
+char	*ft_strchr(const char *str, int c);
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size);
+int	ft_strncmp(char *s1, char *s2, unsigned int n);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
+char	*ft_strrchr(const char *s, int c);
+char	ft_tolower(char c);
+char	ft_toupper(char c);
 
 #endif
